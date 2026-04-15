1b77a022c660e5ff7bc57417dea2bf8f55380479
derlustige47 <sav_47@bk.ru>
2026-04-15 22:51:47 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,13 @@
 import os
-import time
+import asyncio
 from telegram import Bot
-import schedule
 
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
-bot = Bot(token=TOKEN)
+async def main():
+    bot = Bot(token=TOKEN)
+    await bot.send_message(chat_id=CHAT_ID, text="Шопоголик запущен и работает ✅")
 
-def send_message(text):
-    try:
-        bot.send_message(CHAT_ID, text)
-        print("Отправлено:", text)
-    except Exception as e:
-        print("Ошибка отправки:", e)
-
-def check():
-    send_message("Шопоголик запущен и работает ")
-
-schedule.every(3).minutes.do(check)
-
-print("Шопоголик запущен...")
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+if __name__ == "__main__":
+    asyncio.run(main())
