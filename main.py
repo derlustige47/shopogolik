bafb5fceaa7c6e7b092b9dad1c12af11e4e9d80b
derlustige47 <sav_47@bk.ru>
2026-04-15 23:06:08 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import os
 from telegram import Update
-from telegram.ext import Application, CommandHandler
+from telegram.ext import Application, CommandHandler, ContextTypes
 
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
-async def start(update: Update, context):
-    await update.message.reply_text("Шопоголик запущен и готов мониторить объявления ")
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Шопоголик запущен и работает. Пиши, что искать.")
 
 async def main():
     app = Application.builder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-    print("Бот запущен...")
+    
+    print("Шопоголик запущен...")
     await app.run_polling()
 
 if __name__ == "__main__":
