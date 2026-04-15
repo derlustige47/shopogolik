0b7079154efbf1b4a8deae1bd257c0db6dd4556d
derlustige47 <sav_47@bk.ru>
2026-04-15 22:56:51 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import os
-import asyncio
-from telegram import Bot
+from telegram import Update
+from telegram.ext import Application, CommandHandler
 
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
+async def start(update: Update, context):
+    await update.message.reply_text("Шопоголик запущен и готов мониторить объявления ")
+
 async def main():
-    bot = Bot(token=TOKEN)
-    await bot.send_message(chat_id=CHAT_ID, text="Шопоголик запущен и работает ✅")
+    app = Application.builder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    print("Бот запущен...")
+    await app.run_polling()
 
 if __name__ == "__main__":
+    import asyncio
     asyncio.run(main())
